52b039cde2b14275129d9bf1e4386ae297611766
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-19 23:57:21 +0530

    Added: Repeat till the player reaches the winning position 100

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,25 +1,23 @@
 <?php
     function snakeAndLadder(){
-        //UC1
         $playerPosition = 0; //initial positin of player
 
-        //UC2
-        $rollDie = rand(1, 6); //to generate random numbers between 1 to 6
+        //repeat till player reaches to the winning position
+        while($playerPosition <= 100){
+            $rollDie = rand(1, 6); //to generate random numbers between 1 to 6
         
-        //UC3
-        $option = rand(1,3); //random numbers to choose option for player like no play, snake and ladder
-        if($option == 1){
-            $playerPosition = $playerPosition;
-            echo $playerPosition;
+            $option = rand(1,3); //random numbers to choose option for player like no play, snake and ladder
+            if($option == 1){
+                $playerPosition = $playerPosition;
+            }
+            elseif($option == 2){
+                $playerPosition += $rollDie;
+            }
+            else{
+                $playerPosition -= $rollDie;
+            }
         }
-        elseif($option == 2){
-            $playerPosition += $rollDie;
-            echo $playerPosition;
-        }
-        else{
-            $playerPosition -= $rollDie;
-            echo $playerPosition; 
-        }
+        echo "Player Position :" . $playerPosition;
     }
     snakeAndLadder();
 ?>
