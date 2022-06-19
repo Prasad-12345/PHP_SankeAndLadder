4453a54cd7ead0618bfa433098c29b091be886c2
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-19 23:41:41 +0530

    Added: Player rolls the die to get a number between 1 to 6

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -2,5 +2,10 @@
     function snakeAndLadder(){
         //UC1
         $initialPosition = 0;
+
+        //UC2
+        $playerPosition = rand(1, 6);
+        echo $playerPosition;
     }
+    snakeAndLadder();
 ?>
