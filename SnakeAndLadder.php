f5a69c15b32451e7a6b5793532eeeb2aa5063029
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-19 23:51:40 +0530

    Added: Player check for option like no play. snake or ladder

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,11 +1,25 @@
 <?php
     function snakeAndLadder(){
         //UC1
-        $initialPosition = 0;
+        $playerPosition = 0; //initial positin of player
 
         //UC2
-        $playerPosition = rand(1, 6);
-        echo $playerPosition;
+        $rollDie = rand(1, 6); //to generate random numbers between 1 to 6
+        
+        //UC3
+        $option = rand(1,3); //random numbers to choose option for player like no play, snake and ladder
+        if($option == 1){
+            $playerPosition = $playerPosition;
+            echo $playerPosition;
+        }
+        elseif($option == 2){
+            $playerPosition += $rollDie;
+            echo $playerPosition;
+        }
+        else{
+            $playerPosition -= $rollDie;
+            echo $playerPosition; 
+        }
     }
     snakeAndLadder();
 ?>
