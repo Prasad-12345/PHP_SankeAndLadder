748681510b66afbdfa2affe4e1798800426caf9a
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-20 13:35:10 +0530

    Added: Program to to report number of times the dice was playedto win tha game and position after every die roll

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -2,11 +2,12 @@
     class SnakeAndLadder{
         public function getPlayerPosition(){
             $playerPosition = 0; //initial positin of player
-    
+            $count = 0;
+            
             //repeat till player reaches to the winning position
             while($playerPosition <= 100){
                 $rollDie = rand(1, 6); //to generate random numbers between 1 to 6
-            
+                $count = $count + 1;
                 $option = rand(1,3); //random numbers to choose option for player like no play, snake and ladder
                 if($option == 1){
                     $playerPosition = $playerPosition;
@@ -22,7 +23,10 @@
                 if($playerPosition > 100){
                     $extraPoints = $playerPosition - 100;
                     $playerPosition = $playerPosition - $extraPoints;
-                }
+                } 
+                //To print number of times the dice was thrown and the position after every die roll
+                echo "Count :" . $count . "|" . "Player Position :" . $playerPosition . "\n";
+
                 if($playerPosition == 100){
                     break;
                 }
