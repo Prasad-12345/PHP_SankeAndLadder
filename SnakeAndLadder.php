e53fcaed38299ec434faaa920bb0b2b0cee0d70a
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-20 13:20:59 +0530

    Added: To ensure player gets exact winning position 100

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,23 +1,36 @@
 <?php
-    function snakeAndLadder(){
-        $playerPosition = 0; //initial positin of player
+    class SnakeAndLadder{
+        public function getPlayerPosition(){
+            $playerPosition = 0; //initial positin of player
+    
+            //repeat till player reaches to the winning position
+            while($playerPosition <= 100){
+                $rollDie = rand(1, 6); //to generate random numbers between 1 to 6
+            
+                $option = rand(1,3); //random numbers to choose option for player like no play, snake and ladder
+                if($option == 1){
+                    $playerPosition = $playerPosition;
+                }
+                elseif($option == 2){
+                    $playerPosition += $rollDie;
+                }
+                else{
+                    $playerPosition -= $rollDie;
+                }
 
-        //repeat till player reaches to the winning position
-        while($playerPosition <= 100){
-            $rollDie = rand(1, 6); //to generate random numbers between 1 to 6
-        
-            $option = rand(1,3); //random numbers to choose option for player like no play, snake and ladder
-            if($option == 1){
-                $playerPosition = $playerPosition;
+                //To ensure tha player gets to exact winning position 100
+                if($playerPosition > 100){
+                    $extraPoints = $playerPosition - 100;
+                    $playerPosition = $playerPosition - $extraPoints;
+                }
+                if($playerPosition == 100){
+                    break;
+                }
             }
-            elseif($option == 2){
-                $playerPosition += $rollDie;
-            }
-            else{
-                $playerPosition -= $rollDie;
-            }
+            echo "Player Position :" . $playerPosition;
         }
-        echo "Player Position :" . $playerPosition;
     }
-    snakeAndLadder();
+    //object
+    $obj = new SnakeAndLadder();
+    $obj->getPlayerPosition();
 ?>
