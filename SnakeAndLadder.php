a800998dc321c70596fa349aae5c981599a506ad
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-20 14:36:36 +0530

    Added: Game is player by two player then finally report which player won the game

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,40 +1,74 @@
 <?php
     class SnakeAndLadder{
+        //function to return position after eech rolldie
         public function getPlayerPosition(){
-            $playerPosition = 0; //initial positin of player
-            $count = 0;
-            
-            //repeat till player reaches to the winning position
-            while($playerPosition <= 100){
-                $rollDie = rand(1, 6); //to generate random numbers between 1 to 6
-                $count = $count + 1;
-                $option = rand(1,3); //random numbers to choose option for player like no play, snake and ladder
+            $playerPosition = 0;
+            $rollDie = rand(1, 6); //to generate random numbers between 1 to 6
+
+            $option = rand(1,3); //random numbers to choose option for player like no play, snake and ladder
                 if($option == 1){
                     $playerPosition = $playerPosition;
                 }
                 elseif($option == 2){
                     $playerPosition += $rollDie;
+                    $additionalPoints = self::getPlayerPosition();
+                    $playerPosition = $playerPosition + $additionalPoints;
                 }
                 else{
                     $playerPosition -= $rollDie;
                 }
+            return $playerPosition;
+        }
+
+        public function getWinner(){
+            $player1Position = 0; //initial position of player
+            $player2Position = 0;
+            
+            //repeat till player reaches to the winning position
+            while($player1Position <= 100 && $player2Position <= 100){
+                $player1Position = self::getPlayerPosition() + $player1Position;
+
+                //to ensure player should not go in negative position
+                if($player1Position < 0){
+                    $player1Position = 0;
+                }
 
                 //To ensure tha player gets to exact winning position 100
-                if($playerPosition > 100){
-                    $extraPoints = $playerPosition - 100;
-                    $playerPosition = $playerPosition - $extraPoints;
+                if($player1Position > 100){
+                    $extraPoints = $player1Position - 100;
+                    $player1Position = $player1Position - $extraPoints;
                 } 
-                //To print number of times the dice was thrown and the position after every die roll
-                echo "Count :" . $count . "|" . "Player Position :" . $playerPosition . "\n";
 
-                if($playerPosition == 100){
+                //echo "Count :" . $count . "|" . "Player Position :" . $player1Position . "\n";
+
+                if($player1Position == 100){
+                    echo "Player1 win the game \n";
+                    break;
+                }
+
+                $player2Position = self::getplayerPosition() + $player2Position;
+
+                //to ensure player should not go in negative position
+                if($player2Position < 0){
+                    $player2Position = 0; 
+                }
+
+                //To ensure tha player gets to exact winning position 100
+                if($player2Position > 100){
+                    $extraPoints2 = $player2Position - 100;
+                    $player2Position = $player2Position - $extraPoints2;
+                } 
+
+                if($player2Position == 100){
+                    echo "Player2 win the game \n";
                     break;
                 }
             }
-            echo "Player Position :" . $playerPosition;
+            echo "Player1 Position :" . $player1Position . "\n";
+            echo "Player2 Position :" . $player2Position;
         }
     }
     //object
     $obj = new SnakeAndLadder();
-    $obj->getPlayerPosition();
+    $obj->getWinner();
 ?>
